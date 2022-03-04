6aef5ddff82ec7b6acaad301d0a41547f3ca2db9
aliu8606 <adotliu34@gmail.com>
2022-03-04 10:59:54 -0500

    Initial commit

---
+++ src/Algorithm2DArrays.java
@@ -0,0 +1,34 @@
+public class Algorithm2DArrays
+{
+    public class Algorithm2DArrays
+    {
+        /** Return true if target is found in the 2D array arr, false otherwise.
+         *  For example, if arr is {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}
+         *  and target is 5, this method returns true. If target is 15, returns false
+         *
+         *  @param arr  2D array of ints
+         *  @param target  target value to be searched for in arr
+         *  @return  true if target is found at least once in arr; false otherwise
+         */
+        public static boolean isFound(int[][] arr, int target)
+        { /* implement this method! */ }
+
+        public static int countElementsWithSubstring(String[][] arr, String searchStr)
+
+        { /* implement this method! */ }
+
+        public static int replaceEvensWithZero(int[][] arr)
+        { /* implement this method! */ }
+
+        public static int sumForColumn(int[][] arr, int col)
+        { /* implement this method! */ }
+
+        public static int[][] printNumberGrid(int rows, int cols)
+        { /* implement this method! */ }
+
+        public static String[][] gridOfXandO(int n)
+        { /* implement this method! */ }
+
+        public static ArrayList<String> findStringsOfLength(String[][] wordChart, int len)
+        { /* implement this method! */ }
+    }
